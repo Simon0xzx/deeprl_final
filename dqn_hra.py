7df3f104de759d99c5d947cf70db562907072eb7
simon0xzx <simon0xzx@gmail.com>
2017-11-06 20:12:15 -0800

    Cleaning the documentation and adding log during the training loop

--- dqn_hra.py
+++ dqn_hra.py
@@ -2,7 +2,7 @@ import sys
 import os.path as path
 import os, joblib, pickle
 import gym.spaces
-import itertools, time
+import itertools, time, logz, inspect
 import numpy as np
 import random
 import tensorflow                as tf
@@ -31,8 +31,23 @@ def learn(env,
     assert type(env.observation_space) == gym.spaces.Box
     assert type(env.action_space)      == gym.spaces.Discrete
 
-    times, mean_ep_rewards, best_ep_rewards = [], [], []
-
+    # Log the progress during the trainining
+    start = time.time()
+    logdir = 'pacman_hra_' + time.strftime("%d-%m-%Y_%H-%M-%S")
+    logdir = os.path.join('hra_result', logdir)
+    if not(os.path.exists(logdir)):
+        os.makedirs(logdir)
+    logz.configure_output_dir(logdir)
+    args = inspect.getargspec(train_PG)[0]
+    locals_ = locals()
+    params = {k: locals_[k] if k in locals_ else None for k in args}
+    logz.save_params(params)
+    file_path = "rha_result"
+    time_name = path.join(file_path, "dqn_rha_t.dat")
+    mean_name = path.join(file_path, "dqn_rha_mean.dat")
+    best_name = path.join(file_path, "dqn_rha_best.dat")
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
 
     img_h, img_w, img_c = env.observation_space.shape
     input_shape = (img_h, img_w, frame_history_len * img_c)
@@ -62,24 +77,6 @@ def learn(env,
     obs_t_float   = tf.cast(obs_t_ph,   tf.float32) / 255.0
     obs_tp1_float = tf.cast(obs_tp1_ph, tf.float32) / 255.0
 
-    # Here, you should fill in your own code to compute the Bellman error. This requires
-    # evaluating the current and next Q-values and constructing the corresponding error.
-    # TensorFlow will differentiate this error for you, you just need to pass it to the
-    # optimizer. See assignment text for details.
-    # Your code should produce one scalar-valued tensor: total_error
-    # This will be passed to the optimizer in the provided code below.
-    # Your code should also produce two collections of variables:
-    # q_func_vars
-    # target_q_func_vars
-    # These should hold all of the variables of the Q-function network and target network,
-    # respectively. A convenient way to get these is to make use of TF's "scope" feature.
-    # For example, you can create your Q-function network with the scope "q_func" like this:
-    # <something> = q_func(obs_t_float, num_actions, scope="q_func", reuse=False)
-    # And then you can obtain the variables like this:
-    # q_func_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='q_func')
-    # Older versions of TensorFlow may require using "VARIABLES" instead of "GLOBAL_VARIABLES"
-    ######
-
     q_val = q_func(obs_t_float, num_actions, scope="q_func", reuse=False)
     q_food, q_avoid, q_fruit, q_eat = q_val
     target_val = q_func(obs_tp1_float, num_actions, scope="target_q_func", reuse=False)
@@ -102,8 +99,6 @@ def learn(env,
     avoid_error = tf.reduce_mean(tf.losses.huber_loss(y_avoid_t_val, q_act_avoid_t_val))
     fruit_error = tf.reduce_mean(tf.losses.huber_loss(y_fruit_t_val, q_act_fruit_t_val))
     eat_error = tf.reduce_mean(tf.losses.huber_loss(y_eat_t_val, q_act_eat_t_val))
-
-    ######
 
     # construct optimization op (with gradient clipping)
     learning_rate = tf.placeholder(tf.float32, (), name="learning_rate")
@@ -136,6 +131,7 @@ def learn(env,
     best_mean_episode_reward = -float('inf')
     last_obs = env.reset()
     LOG_EVERY_N_STEPS = 10000
+    times, mean_ep_rewards, best_ep_rewards = [], [], []
 
     for t in itertools.count():
         ### 1. Check stopping criterion
@@ -143,37 +139,6 @@ def learn(env,
             break
 
         ### 2. Step the env and store the transition
-        # At this point, "last_obs" contains the latest observation that was
-        # recorded from the simulator. Here, your code needs to store this
-        # observation and its outcome (reward, next observation, etc.) into
-        # the replay buffer while stepping the simulator forward one step.
-        # At the end of this block of code, the simulator should have been
-        # advanced one step, and the replay buffer should contain one more
-        # transition.
-        # Specifically, last_obs must point to the new latest observation.
-        # Useful functions you'll need to call:
-        # obs, reward, done, info = env.step(action)
-        # this steps the environment forward one step
-        # obs = env.reset()
-        # this resets the environment if you reached an episode boundary.
-        # Don't forget to call env.reset() to get a new observation if done
-        # is true!!
-        # Note that you cannot use "last_obs" directly as input
-        # into your network, since it needs to be processed to include context
-        # from previous frames. You should check out the replay buffer
-        # implementation in dqn_utils.py to see what functionality the replay
-        # buffer exposes. The replay buffer has a function called
-        # encode_recent_observation that will take the latest observation
-        # that you pushed into the buffer and compute the corresponding
-        # input that should be given to a Q network by appending some
-        # previous frames.
-        # Don't forget to include epsilon greedy exploration!
-        # And remember that the first time you enter this loop, the model
-        # may not yet have been initialized (but of course, the first step
-        # might as well be random, since you haven't trained your net...)
-
-        #####
-
         idx = replay_buffer.store_frame(last_obs, rha_shape=4)
         epsilon = exploration.value(t)
 
@@ -192,53 +157,11 @@ def learn(env,
         if done: obs = env.reset()
         last_obs = obs
 
-        #####
-
-        # at this point, the environment should have been advanced one step (and
-        # reset if done was true), and last_obs should point to the new latest
-        # observation
-
         ### 3. Perform experience replay and train the network.
-        # note that this is only done if the replay buffer contains enough samples
-        # for us to learn something useful -- until then, the model will not be
-        # initialized and random actions should be taken
         if (t > learning_starts and
                 t % learning_freq == 0 and
                 replay_buffer.can_sample(batch_size)):
-            # Here, you should perform training. Training consists of four steps:
-            # 3.a: use the replay buffer to sample a batch of transitions (see the
-            # replay buffer code for function definition, each batch that you sample
-            # should consist of current observations, current actions, rewards,
-            # next observations, and done indicator).
-            # 3.b: initialize the model if it has not been initialized yet; to do
-            # that, call
-            #    initialize_interdependent_variables(session, tf.global_variables(), {
-            #        obs_t_ph: obs_t_batch,
-            #        obs_tp1_ph: obs_tp1_batch,
-            #    })
-            # where obs_t_batch and obs_tp1_batch are the batches of observations at
-            # the current and next time step. The boolean variable model_initialized
-            # indicates whether or not the model has been initialized.
-            # Remember that you have to update the target network too (see 3.d)!
-            # 3.c: train the model. To do this, you'll need to use the train_fn and
-            # total_error ops that were created earlier: total_error is what you
-            # created to compute the total Bellman error in a batch, and train_fn
-            # will actually perform a gradient step and update the network parameters
-            # to reduce total_error. When calling session.run on these you'll need to
-            # populate the following placeholders:
-            # obs_t_ph
-            # act_t_ph
-            # rew_t_ph
-            # obs_tp1_ph
-            # done_mask_ph
-            # (this is needed for computing total_error)
-            # learning_rate -- you can get this from optimizer_spec.lr_schedule.value(t)
-            # (this is needed by the optimizer to choose the learning rate)
-            # 3.d: periodically update the target network by calling
-            # session.run(update_target_fn)
-            # you should update every target_update_freq steps, and you may find the
-            # variable num_param_updates useful for this (it was initialized to 0)
-            #####
+
             obs_t_batch, act_t_batch, rew_t_batch, obs_tp1_batch, done_mask_batch =  replay_buffer.sample(batch_size)
             rew_food_t_batch = rew_t_batch[:, 0]
             rew_fruit_t_batch = rew_t_batch[:, 1]
@@ -330,27 +253,22 @@ def learn(env,
             mean_ep_rewards.append(mean_episode_reward)
             best_ep_rewards.append(best_mean_episode_reward)
 
-            log_dict = {"time":times, "mean": mean_ep_rewards, "best": best_ep_rewards}
-            f = open("output_batch{}.pkl".format(batch_size), "wb")
-            pickle.dump(log_dict, f)
-            f.close()
+            joblib.dump(value=times, filename = time_name, compress=3)
+            joblib.dump(value=mean_ep_rewards, filename = mean_name, compress=3)
+            joblib.dump(value=best_ep_rewards, filename = best_name, compress=3)
 
-            print("Timestep %d" % (t,))
-            print("mean reward (100 episodes) %f" % mean_episode_reward)
-            print("best mean reward %f" % best_mean_episode_reward)
-            print("episodes %d" % len(episode_rewards))
-            print("exploration %f" % exploration.value(t))
-            print("learning_rate %f" % optimizer_spec.lr_schedule.value(t))
+            logz.log_tabular("Training Time", time.time() - start)
+            logz.log_tabular("Loss", train_loss)
+            logz.log_tabular("Iteration", t)
+            logz.log_tabular("Mean Reward (/100ep)", mean_episode_reward)
+            logz.log_tabular("Best Mean Reward", best_mean_episode_reward)
+            logz.log_tabular("Episodes", len(episode_rewards))
+            logz.log_tabular("Exploration", exploration.value(t))
+            logz.log_tabular("Learning Rate", optimizer_spec.lr_schedule.value(t))
+            logz.dump_tabular()
+            logz.pickle_tf_vars()
+
             sys.stdout.flush()
 
-    file_path = "rha_result"
-    time_name = path.join(file_path, "dqn_rha_t.dat")
-    mean_name = path.join(file_path, "dqn_rha_mean.dat")
-    best_name = path.join(file_path, "dqn_rha_best.dat")
-    if not os.path.exists(file_path):
-        os.makedirs(file_path)
-    joblib.dump(value=times, filename = time_name, compress=3)
-    joblib.dump(value=mean_ep_rewards, filename = mean_name, compress=3)
-    joblib.dump(value=best_ep_rewards, filename = best_name, compress=3)
 
     return times, mean_ep_rewards, best_ep_rewards
